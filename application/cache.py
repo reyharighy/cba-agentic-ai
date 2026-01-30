08d78c1b32fb6ddbc9f6ff7ac4424ce9be484118
reyharighy <arighymoch@gmail.com>
2026-01-30 15:49:31 +0700

    Small Consideration
    Move all all functionalities that use Streamlit built-in tool in cache package into application layer.

---
+++ application/cache.py
@@ -0,0 +1,190 @@
+# pyright: reportMissingTypeStubs=false
+
+# standard
+from importlib.abc import Loader
+from importlib.util import (
+    module_from_spec,
+    spec_from_file_location,
+)
+from pathlib import Path
+from types import ModuleType
+from typing import Literal
+
+# third-party
+from langchain_core.language_models import BaseChatModel
+from langgraph.graph.state import CompiledStateGraph
+
+# internal
+from agent import (
+    Graph,
+    Context,
+    State,
+)
+from context.database import (
+    ContextManager,
+    external_db_url,
+)
+from context.system_prompts import prompt_dict
+from language_model.provider import groq_gpt_120b_high
+from memory.database import (
+    MemoryManager,
+    internal_db_url,
+)
+from util import st_cache
+
+
+@st_cache("Setting up application data and resources", "data")
+def cold_start() -> None:
+    """
+    Docstring for cold_start
+    """
+    load_memory_manager()
+    load_context_manager()
+    load_language_model()
+    load_graph()
+    load_prompts_set()
+    load_analytical_sandbox_bootstrap()
+    load_infographic_sandbox_bootstrap()
+
+
+@st_cache("Loading database manager", "resource")
+def load_memory_manager() -> MemoryManager:
+    """
+    Docstring for load_memory_manager
+
+    :return: Description
+    :rtype: MemoryManager
+    """
+    return MemoryManager(internal_db_url)
+
+
+@st_cache("Loading context manager", "resource")
+def load_context_manager() -> ContextManager:
+    """
+    Docstring for load_context_manager
+
+    :return: Description
+    :rtype: ContextManager
+    """
+    return ContextManager(external_db_url)
+
+
+@st_cache("Loading language models", "resource")
+def load_language_model() -> BaseChatModel:
+    """
+    Docstring for load_language_model
+
+    :return: Description
+    :rtype: BaseChatModel
+    """
+    return groq_gpt_120b_high
+
+
+@st_cache("Loading graph", "resource")
+def load_graph() -> CompiledStateGraph[State, Context]:
+    """
+    Docstring for load_graph
+
+    :return: Description
+    :rtype: CompiledStateGraph[State, Context, State, State]
+    """
+    context_manager: ContextManager = load_context_manager()
+    memory_manager: MemoryManager = load_memory_manager()
+    language_model: BaseChatModel = load_language_model()
+
+    graph = Graph(
+        context_manager=context_manager,
+        memory_manager=memory_manager,
+        language_model=language_model,
+    )
+
+    return graph.build_graph()
+
+
+@st_cache("Loading prompts set", "data")
+def load_prompts_set() -> dict[str, str]:
+    """
+    Docstring for load_prompts_set
+
+    :return: Description
+    :rtype: dict[str, str]
+    """
+    return prompt_dict
+
+
+@st_cache("Loading analytical sandbox bootstrap", "data")
+def load_analytical_sandbox_bootstrap() -> dict[Literal["descriptive", "diagnostic", "predictive", "inferential"], str]:
+    """
+    Docstring for load_analytical_sandbox_bootstrap
+
+    :return: Description
+    :rtype: dict[Literal['descriptive', 'diagnostic', 'predictive', 'inferential'], str]
+    """
+    ignore_warnings: str = "import warnings\n"
+    ignore_warnings += "warnings.filterwarnings('ignore')\n"
+    pandas: str = "import pandas as pd\n"
+    numpy: str = "import numpy as np\n"
+    scipy: str = "import scipy\n"
+    sklearn: str = "import sklearn\n"
+    df_load: str = "df = pd.read_csv('dataset.csv')\n"
+    df_load += "\n" + "for column in df.columns:"
+    df_load += "\n\t" + "if pd.api.types.is_object_dtype(df[column]):"
+    df_load += "\n\t\t" + "try:"
+    df_load += "\n\t\t\t" + "df[column] = pd.to_datetime(df[column])"
+    df_load += "\n\t\t" + "except Exception as _:"
+    df_load += "\n\t\t\t" + "pass\n"
+
+    descriptive: str = ignore_warnings + pandas + numpy + df_load
+    diagnostic: str = ignore_warnings + pandas + numpy + scipy + df_load
+    predictive: str = ignore_warnings + pandas + numpy + scipy + df_load
+    inferential: str = ignore_warnings + pandas + numpy + sklearn + df_load
+
+    return {
+        "descriptive": descriptive,
+        "diagnostic": diagnostic,
+        "predictive": predictive,
+        "inferential": inferential,
+    }
+
+
+@st_cache("Loading infographic sandbox bootstrap", "data")
+def load_infographic_sandbox_bootstrap() -> str:
+    """
+    Docstring for load_infographic_sandbox_bootstrap
+
+    :return: Description
+    :rtype: str
+    """
+    warnings: str = "import warnings\n"
+    pandas: str = "import pandas as pd\n"
+    numpy: str = "import numpy as np\n"
+    graph_objects: str = "import plotly.graph_objects as go\n"
+    express: str = "import plotly.express as px\n\n"
+    ignore_warnings: str = "warnings.filterwarnings('ignore')\n"
+    df_load: str = "df = pd.read_csv('dataset.csv')\n"
+    df_transform: str = "\n" + "for column in df.columns:"
+    df_transform += "\n\t" + "if pd.api.types.is_object_dtype(df[column]):"
+    df_transform += "\n\t\t" + "try:"
+    df_transform += "\n\t\t\t" + "df[column] = pd.to_datetime(df[column])"
+    df_transform += "\n\t\t" + "except Exception as _:"
+    df_transform += "\n\t\t\t" + "pass\n\n"
+
+    return warnings + pandas + numpy + graph_objects + express + ignore_warnings + df_load + df_transform
+
+
+@st_cache("Loading infographic object", "resource")
+def load_infographic(infographic_object_file_path: Path) -> tuple[Loader | None, ModuleType | None]:
+    """
+    Docstring for load_infographic
+
+    :param infographic_object_file_path: Description
+    :type infographic_object_file_path: Path
+    :return: Description
+    :rtype: tuple[Loader | None, ModuleType | None]
+    """
+    spec = spec_from_file_location(
+        name=infographic_object_file_path.stem,
+        location=infographic_object_file_path,
+    )
+
+    return (spec.loader, module_from_spec(spec)) if spec else (None, None)
